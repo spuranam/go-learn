bfe93936034213548145478a8a15485b3491b41d
Satish Puranam <satishpuranam@Satishs-MBP.twmi.rr.com>
2014-12-15 19:23:44 -0500

    add package description

--- 2.packages/package.go
+++ 2.packages/package.go
@@ -1,3 +1,8 @@
+// package is a collection types, variables, constants, functions defined in
+// one or more files. Some of these identifiers are exported, so that other
+// packages can import them.
+
+// Programs starts executing in the main package, specifically in the main function
 package main
 
 import "./calc"
