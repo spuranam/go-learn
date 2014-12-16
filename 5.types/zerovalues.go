469d83f964f2fef52a924c816c7deb8415d7ad58
satish.puranam@gmail.com <satish.puranam@gmail.com>
2014-12-16 16:25:22 -0500

    add commnetray

--- 5.types/zerovalues.go
+++ 5.types/zerovalues.go
@@ -26,7 +26,7 @@ func main() {
 	// allocation with new
 	//------------------------
 	// Let's talk about new first. It's a built-in function that allocates memory,
-	//  but unlike its namesakes in some other languages it does not initialize
+	// but unlike its namesakes in some other languages it does not initialize
 	// the memory, it only zeros it. That is, new(T) allocates zeroed storage for
 	// a new item of type T and returns its address, a value of type *T.
 	// In Go terminology, it returns a pointer to a newly allocated zero value of type T.
