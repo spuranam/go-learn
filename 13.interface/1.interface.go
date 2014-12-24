eef94d98ecebf08922fc847717150f5da7937424
Satish Puranam <satish.puranam@gmail.com>
2014-12-23 21:29:42 -0500

    complete the sample

--- 13.interface/1.interface.go
+++ 13.interface/1.interface.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // An interface defines behaviour of an object. In go an interface is a type
 // that specifies one or more method signatures collectively called method set.
 // Interfaces are wholly abstract, so it is not possible to instantiate an interface.
@@ -13,29 +18,90 @@ package main
 // of methods as well as the methods it actually has. This is why the interface{}
 // type can be used for anyvalue.
 
-type transporter interface {
-	speedup(int)
-	slowdown(int)
-	wheelcount() int
+// A type doesn’t have to state explicitly that it implements an interface:
+// interfaces are satisfied implicitly.
+// Multiple types can implement the same interface.
+// A type that implements an interface can also have other functions. A type can
+// implement many interfaces.
+// An interface type can contain a reference to an instance of any of the types
+// that implement the interface (an interface has what is called a dynamic type)
+
+type shaper interface {
+	area() float64
+	perimeter() float64
 }
 
-type car struct {
-	speed  int
-	wheels int
-	make   string
+type enlarger interface {
+	enlarge(float64)
 }
 
-type bike struct {
-	speed  int
-	wheels int
-	make   string
+type circle struct {
+	radius float64
 }
 
-type bus struct {
-	speed  int
-	wheels int
-	make   string
+type rectangle struct {
+	length, width float64
+}
+
+type square struct {
+	rectangle //anonymous field (embedding)
+}
+
+// The methods area and perimeter on types circle, and rectangle are defined
+// with a value reciever since these methods do not mutate (modify) the state of
+// their respective types.
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+// enlarge method is defined with a pointer reciever since it mutates (modifies)
+// the passed values state. If the reciver was defined as value then the chnage
+// to the state of the value will not visible.
+func (c *circle) enlarge(by float64) {
+	c.radius += by
+}
+
+// notice that we have not defined the area and perimeter methods for the square type
+// If an embedded field has methods we can call them on the containing struct,
+// and only the embedded field will be passed as the methods’ receiver.
+func (r rectangle) area() float64 {
+	return r.length * r.width
+}
+
+func (r rectangle) perimeter() float64 {
+	return 2 * (r.length + r.width)
+}
+
+func (r *rectangle) enlarge(by float64) {
+	r.length += by
+	r.width += by
+}
+
+// geometry takes any values that satisfy the shaper interface and is able
+// dynamically dispatch the call to correct area and perimeter methods
+func geometry(s shaper) {
+	fmt.Printf("Area of %#v is %.2f\n", s, s.area())
+	fmt.Printf("Perimeter of %#v is %.2f\n", s, s.perimeter())
 }
 
 func main() {
+	c := circle{2.12}
+	r := rectangle{4.3, 3.5}
+	s := square{rectangle{4.6, 4.6}}
+
+	geometry(c)
+	geometry(r)
+	geometry(s)
+
+	fmt.Printf("Before enlarge %#v\n", c)
+	// Since the circle type is addressable Go would automatically modify the
+	// call to enlarge method by passing a pointer to c i.e. it would modify
+	// the method call to read (&c).enlarge(1.1)
+	c.enlarge(1.1)
+	fmt.Printf("After enlarge %#v\n", c)
+
 }
